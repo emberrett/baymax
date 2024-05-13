ad8bb9d03f6f0b0ece06b5540b536930d700af35
Ethan Berrett <ethanb@qualtrics.com>
2024-05-13 17:43:25 -0500

    python script with telegram working

--- README.md
+++ README.md
@@ -10,3 +10,13 @@ Huggingface doesn't make it very clear how exactly to "download models". You wil
 
 
 # Set up Telegram bot
+1. Download Telegram
+2. Send the following messages to @BotFather on Telegram to get your token
+    1. `/start`
+    2. `/newbot`
+    3. `<your_bot_name>`
+3. To get your Telegram chat ID:
+    1. Message your new bot
+    2. Open https://api.telegram.org/bot<your_token>/getUpdates in a browser
+    3. Copy your chat ID
+3. Paste token into your `.env` file
--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 from llama_cpp import Llama
 import os
 from dotenv import load_dotenv
+from telegram import Update
+from telegram.ext import Application, ContextTypes, CommandHandler, MessageHandler, filters
+import logging
 
 load_dotenv()
 
 model_path = os.environ["MODEL_PATH"]
+telegram_token = os.environ["TELEGRAM_TOKEN"]
 gpu_layers = int(os.environ["N_GPU_LAYERS"])
+chat_id = int(os.environ["TELEGRAM_CHAT_ID"])
+in_container = os.path.exists('/.dockerenv')
 
 if 'nt' not in os.name:
-    # if running in container
-    if os.path.exists('/.dockerenv'):
+    if in_container:
         model_path = 'models' + '/' + os.path.basename('model_path')
     else:
         # ensure the path matches unix standard
-        model_path =  os.path.abspath(model_path)
+        model_path = os.path.abspath(model_path)
 
 llm = Llama(
     model_path=model_path,
@@ -21,14 +26,62 @@ llm = Llama(
     n_gpu_layers=gpu_layers
 )
 
-prompt = input("Prompt: ")
-messages = [{"role": "system", "content": "You are a virtual assistant who responds with helpful answers."}]
-while prompt.lower() != 'clear':
-    messages.append({"role": "user", "content": prompt})
-    output = llm.create_chat_completion(messages, max_tokens=128)
-    content = output["choices"][0]["message"]["content"]
-    print(content)
-    messages.append({"role": "assistant", "content": content})
-    prompt = input("Prompt: ")
-    print(messages)
 
+logging.getLogger("httpx").setLevel(logging.WARNING)
+logger = logging.getLogger(__name__)
+
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Echo the user message."""
+
+    await update.message.reply_text(update.message.text)
+
+
+def main() -> None:
+
+    def reset_chat_context():
+        global chat_context
+        chat_context = [{"role": "system",
+                         "content": "You are a virtual assistant who responds with helpful answers."}]
+
+    reset_chat_context()
+
+    application = Application.builder().token(telegram_token).build()
+
+    async def confirm_chat_id(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        if update.message.chat_id == chat_id:
+            pass
+        else:
+            await update.message.reply_text("Unauthorized.")
+
+    async def help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        await update.message.reply_text("/clear will clear chat context. This may be helpful if the bot is running too slowly")
+
+    async def clear(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        logging.info("Clearing chat context...")
+        reset_chat_context()
+        await update.message.reply_text("Chat context cleared.")
+
+    async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        logging.info("User requested ")
+        chat_context.append({"role": "user", "content": update.message.text})
+        output = llm.create_chat_completion(chat_context, max_tokens=128)
+        content = output["choices"][0]["message"]["content"]
+        chat_context.append({"role": "assistant", "content": content})
+        await update.message.reply_text(content)
+
+    application.add_handler(MessageHandler(None, confirm_chat_id))
+
+    application.add_handler(CommandHandler("help", help))
+
+    application.add_handler(CommandHandler("clear", clear))
+
+    application.add_handler(MessageHandler(
+        filters.TEXT & ~filters.COMMAND, reply))
+
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
+
+
+if __name__ == "__main__":
+
+    main()
